dda004f34706df13fe0674d5ca195f02f79e241a
Thomas <boy1.thoomas@gmail.com>
2025-04-03 11:03:46 +0200

    feat(project): immer integration

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -9,23 +9,17 @@ const todoSlice = createSlice({
   initialState,
   reducers: {
     addTodo: (state, action) => {
-      return {
-        ...state,
-        todos: [
-          ...state.todos,
-          { id: Date.now(), texte: action.payload, completed: false },
-        ],
-      };
+      state.todos.push({
+        id: Date.now(),
+        texte: action.payload,
+        completed: false,
+      });
     },
     toggleTodo: (state, action) => {
-      return {
-        ...state,
-        todos: state.todos.map((todo) =>
-          todo.id === action.payload
-            ? { ...todo, completed: !todo.completed }
-            : todo
-        ),
-      };
+      const todo = state.todos.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
     },
   },
 });
